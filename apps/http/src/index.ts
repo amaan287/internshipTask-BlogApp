ad17a6f7bdc9241920a64fb38ce103462f209748
amaan287 <amaanmirza287@gmail.com>
2024-11-13 19:26:04 +0530

    Port corrected

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { router } from "./routes/v1";
-import client from "@repo/db/client";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use("/api/v1", router);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost${PORT}`);
+  console.log(`Server is running in http://localhost${PORT}`);
 });
